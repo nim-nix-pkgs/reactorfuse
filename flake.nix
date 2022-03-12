0806cb68550e0c171b5fb43741c3698168922f2a
hugosenari <hugosenari@gmail.com>
2022-03-12 02:32:12 -0300

    chore: re index reactorfuse

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Filesystem in userspace (FUSE) for Nim (for reactor.nim library)'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."reactorfuse-master".url = "path:./master";
+  inputs."reactorfuse-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."reactorfuse-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Filesystem in userspace (FUSE) for Nim (for reactor.nim library)'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-reactorfuse-master.flake = false;
+  inputs.src-reactorfuse-master.owner = "zielmicha";
+  inputs.src-reactorfuse-master.ref   = "refs/heads/master";
+  inputs.src-reactorfuse-master.repo  = "reactorfuse";
+  inputs.src-reactorfuse-master.type  = "github";
+  
+  inputs."reactor".dir   = "nimpkgs/r/reactor";
+  inputs."reactor".owner = "riinr";
+  inputs."reactor".ref   = "flake-pinning";
+  inputs."reactor".repo  = "flake-nimble";
+  inputs."reactor".type  = "github";
+  inputs."reactor".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."reactor".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-reactorfuse-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-reactorfuse-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
